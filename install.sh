f958c6533849dd87e4d60beb770a8cea45637b97
Sean Ly <seanly@aliyun.com>
2017-09-09 00:43:37 +0800

    fixbug

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 PLUG_VIM_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $PLUG_VIM_URL
 
-if [ ! -d ~/.config/nvim ]; then
+if [ ! -d ~/.config/nvim/plugged ]; then
   mkdir -p ~/.config/nvim/plugged
   ln -s `pwd`/init.vim ~/.config/nvim/init.vim
 else

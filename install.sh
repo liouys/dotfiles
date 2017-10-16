bc99250eb0ea5411d67ecc1e447110793c5e315d
Sean Ly <seanly@aliyun.com>
2017-10-16 14:43:57 +0800

    add install neovim

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 PLUG_VIM_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $PLUG_VIM_URL
 
+yum install -y neovim ctags git
+
 if [ ! -d ~/.config/nvim/plugged ]; then
   mkdir -p ~/.config/nvim/plugged
   ln -s `pwd`/init.vim ~/.config/nvim/init.vim
